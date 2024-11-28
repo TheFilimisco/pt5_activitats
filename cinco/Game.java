62c2109922e99253a62012422c7d78deca38cb6a
Steven Uriarte <54910978l@iespoblenou.org>
2024-11-28 14:14:15 +0100

    Complete game add all functiones tested

--- cinco/Game.java
+++ cinco/Game.java
@@ -1,4 +1,91 @@
 package cinco;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Game {
+    public static void main(String[] args) {
+        ArrayList<Player> listPlayer = new ArrayList<>();
+        Game game = new Game();
+        Scanner input = new Scanner(System.in);
+        boolean runing = true;
+        int topeVectores = 10;
+        initMenuGame(runing,input,game,listPlayer,topeVectores);
+    }
+
+    public static void initMenuGame(boolean runing, Scanner input, Game game, ArrayList<Player> listPlayer, int topeVectores) {
+        while (runing) {
+            game.imprimirMenuString();
+            int option = input.nextInt();
+            input.nextLine();
+            switch (option){
+                case 1:
+                    game.addNuevoJugador(input,listPlayer,topeVectores);
+                    break;
+                case 2:
+                    game.deleteJugador(listPlayer);
+                    break;
+                case 3:
+                    game.reiniciarPartida(listPlayer);
+                    break;
+                case 4:
+                    game.mostrarTodosJugadores(listPlayer);
+                    break;
+                case 5:
+                    runing = false;
+                    break;
+            }
+        }
+
+    }
+
+    public void imprimirMenuString(){
+        System.out.println("""
+                    ========Menu de Opciones======
+                    1. Añadir nuevo jugador
+                    2. Eliminar jugador
+                    3. Reiniciar partida
+                    4. Mostrar Jugadores
+                    5. Salir
+                    """);
+    }
+
+    public void addNuevoJugador(Scanner input, ArrayList<Player> listPlayer, int numeroMaximo) {
+        System.out.println("======Añadir Nuevo Jugador===");
+        System.out.print("Añadir el nombre:");
+        String nombre = input.nextLine();
+        if (listPlayer.size() == numeroMaximo) {
+            System.out.println("No se puede agregar mas Jugadores");
+            return;
+        }
+        listPlayer.add( new Player(nombre,new Position()));
+        System.out.println("Jugador creado correctamente");
+    }
+
+    public void deleteJugador(ArrayList<Player> listPlayer) {
+        System.out.println("=======Eliminar Jugador=========");
+        listPlayer.removeLast();
+        System.out.println("Jugador ultimo eliminado correctamente");
+    }
+
+    public void reiniciarPartida(ArrayList<Player> listPlayer) {
+        System.out.println("========Reiniciar Partida=========");
+        listPlayer.clear();
+        System.out.println("Partida reiniciada correctamente");
+    }
+
+    public void mostrarTodosJugadores(ArrayList<Player> listPlayer) {
+        System.out.println("=========Mostrar Jugadores========");
+        for (Player player : listPlayer){
+            System.out.println(player);
+        }
+    }
+
+
+
+
+
 }
+
+
+
--- cinco/Player.java
+++ cinco/Player.java
@@ -1,4 +1,81 @@
 package cinco;
 
 public class Player {
+    private  String nombre;
+    private Position position;
+    private final int RIGHT=1;
+    private final int LEFT=1;
+    private final int UP=1;
+    private final int DOWN=1;
+
+    public Player(String nombre, Position position) {
+        this.nombre = nombre;
+        this.position = position;
+    }
+
+    public Player() {
+        this.nombre = "Anonimo";
+        this.position = new Position();
+    }
+
+
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public void moveRight(){
+        this.getPosition().incX(RIGHT);
+    }
+
+    public void moveLeft(){
+        this.getPosition().decX(LEFT);
+    }
+
+    public void jump(){
+        this.getPosition().incY(UP);
+    }
+
+    public void fall(){
+        this.getPosition().decY(DOWN);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "nombre='" + nombre + '\''+ ", "+  position +
+                '}';
+    }
+
+//    public static void main(String[] args) {
+//        Player player = new Player("Joaquin", new Position());
+//        Player anonimo = new Player();
+//        System.out.println(anonimo);
+//        System.out.println(player);
+//        player.moveRight();
+//        System.out.println(player);
+//        player.moveLeft();
+//        System.out.println(player);
+//        player.jump();
+//        System.out.println(player);
+//        player.jump();
+//        System.out.println(player);
+//        player.fall();
+//        System.out.println(player);
+//
+//    }
 }
